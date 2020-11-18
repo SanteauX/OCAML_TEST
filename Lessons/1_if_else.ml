ee4a3cdc20e47f9d2b689d8be9ea22dec96cbac0
SanteauX <hugo.joncour.k2o@gmail.com>
2020-11-18 03:03:54 +0100

    if and for loops

---
+++ Lessons/1_if_else.ml
@@ -0,0 +1,11 @@
+let i = 9 and j = 10 in
+
+if i < j || false then
+    (
+      if j > i then 5 else 6
+    )
+  else
+    begin
+      let f = 3 in
+      f+4
+    end;;
---
+++ Lessons/2_loop.ml
@@ -0,0 +1,28 @@
+(*
+for variable = start_value to end_value do
+    expression
+done
+
+for variable = start_value down to end_value do
+    expression
+done
+
+= checking structural equality
+
+== checking physical equality
+
+*)
+
+for i = 0 to 10 do
+    print_string "value of i is ";
+    print_int i;
+    print_string "\n";
+done;;
+print_string "\n";
+
+for i = 10 downto 0 do
+    print_string "value of i is:";
+    print_int i;
+    print_string "\n";
+done;;
+
